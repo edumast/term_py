da10631ed728b4e2620114c82ba4a4ccbbaeb5fe
edumast <euedu90@gmail.com>
2022-02-19 18:26:51 -0300

    up

--- tp.py
+++ tp.py
@@ -2,21 +2,31 @@ import os
 from databased import database
 pasta = 'databased/'
 init=True
+aspa=False    
 print("welcome "+database.name_usr)
 while( init == True ):
     command=input("C:")
     if ( command == "change" ):
+        change_info="__er!n__"
         change_info=input("what changed?:")
+        valor_new=input("valor:")
         if ( change_info == "age" ):
-            age_new=input("age:")
-            with open("databased/database.py", "r") as file:
-	            x = file.read()
-	
-            with open("databased/database.py", "w") as file:
-    
-                x = x.replace("age_usr="+str(database.age_usr),"age_usr="+age_new,1)
-            with open('databased/database.py', 'w') as fd:
-                fd.write(x)
+            change_var="age_usr"
+            type_change=(database.age_usr)
+        if ( change_info == "name" ):
+            change_var="name_usr"
+            type_change=("'"+database.name_usr+"'")
+            aspa=True
+        if ( aspa == True ):
+            valor_new="'"+valor_new+"'"
+        with open("databased/database.py", "r") as file:
+	        x = file.read()
+        with open("databased/database.py", "w") as file:
+            x = x.replace(str(change_var)+"="+str(type_change),str(change_var)+"="+valor_new,1)
+        with open('databased/database.py', 'w') as fd:
+            fd.write(x)
+
+
     elif ( command == "list" ):
         for diretorio,subpastas,arquivos in os.walk(pasta):
             for arquivo in arquivos:
